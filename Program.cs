ae311cefd97015943b8a8d4946eb7aeea3165a2b
NataIlyicheva <114134104+NataIlyicheva@users.noreply.github.com>
2022-12-05 18:37:49 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Write("Введите несколько элементов через пробел ");
 string str = new string (Console.ReadLine());
 string[] array = str.Split(new char[] {' '});
-
+for (int i = 0; i < array.Length; i++)
+{
+   int input = Convert.ToInt32(array[i].Length);
+   if (input <= 3)
+   {
+      Console.WriteLine(array[i]);
+      count++;
+   }
+}
