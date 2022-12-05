f29cc292b6fe23fe7a78485bb1e03e6b5d0d69de
Natali <n7483205@ya.ru>
2022-12-05 18:29:26 +0700

    Loop initiated

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
 ﻿Console.Write("Введите несколько элементов через пробел ");
 string str = new string (Console.ReadLine());
 string[] array = str.Split(new char[] {' '});
+int count = 0;
 
+for (int i = 0; i < array.Length; i++)
+{
+   int input = Convert.ToInt32(array[i].Length);
+   if (input <= 3)
+   {
+      Console.WriteLine(array[i]);
+      count++;
+   }
+}
